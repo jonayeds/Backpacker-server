67e6b05ccb55be2320a4febac8dcfd53b97054ad
jonayeds <jonayeeds@gmail.com>
2024-06-02 13:58:53 +0600

    added user to database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
-app.use(cors())
+app.use(cors())  
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
