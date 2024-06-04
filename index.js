eba3a9998a80af655d031bdbd406dfe722f69d54
jonayeds <jonayeeds@gmail.com>
2024-06-04 17:14:08 +0600

    added managge users

--- index.js
+++ index.js
@@ -99,6 +99,10 @@ async function run() {
       const result = await usersCollection.findOne(query)
       res.send(result)
     })
+    app.get('/users', async(req, res)=> {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
     app.get('/guide', async(req, res)=>{
       const filter = {role : 'guide'}
       const result = await usersCollection.find(filter).toArray()
@@ -129,7 +133,7 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updatedUser)
       res.send(result)
     })
-    app.put('/users/update/:email', async(req , res)=>{
+    app.put('/users/guide/update/:email', async(req , res)=>{
       const email = req.params.email
       const filter = {email: email}
       const query = req.body
@@ -138,6 +142,19 @@ async function run() {
           phone : query.phone,
           experience : query.experience,
           education : query.education
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedUser)
+      res.send(result)
+    })
+    app.put('/users/updateRole/:id', async(req , res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id) }
+      const query = req.body
+      const updatedUser = {
+        $set : {
+          role : query.role,
+          updated : query.updated,
         }
       }
       const result = await usersCollection.updateOne(filter, updatedUser)
